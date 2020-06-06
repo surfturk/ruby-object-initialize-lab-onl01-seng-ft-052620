632cc8de3daf90cb196231e27bea22af39a4b8e7
Berkley Brown <berkleydeane@gmail.com>
2020-06-06 16:48:52 +0000

    Automatically backed up by Learn

--- lib/dog.rb
+++ lib/dog.rb
@@ -1,7 +1,11 @@
 require = "pry"
-attr_accessor :name, :breed
+
 
 class Dog
+  attr_accessor :name, :breed
+  
+  
+  
   def initialize(name, breed = "Mutt")
     @breed = breed
     @name = name 
