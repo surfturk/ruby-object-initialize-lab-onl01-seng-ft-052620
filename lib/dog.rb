09c59c0f1a2ffb6b2632498657026cccb1ce5eb3
Berkley Brown <berkleydeane@gmail.com>
2020-06-06 16:48:27 +0000

    Automatically backed up by Learn

--- lib/dog.rb
+++ lib/dog.rb
@@ -8,20 +8,5 @@ class Dog
   end
  
  
- def name=(name)
-    @name = name
-end 
 
-def name 
-  @name 
-end
- 
- 
-  def breed=(breed)
-    @breed = breed
-  end
- 
-  def breed
-    @breed
-  end
 end
